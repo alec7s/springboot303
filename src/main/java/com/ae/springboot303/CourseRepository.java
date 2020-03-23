c0f3928fc3577a9e14186c9bc6518993381a13ee
Alex Evans <alex.c.evans1@gmail.com>
2020-03-23 19:10:59 -0400

    Initial commit

---
+++ src/main/java/com/ae/springboot303/CourseRepository.java
@@ -0,0 +1,5 @@
+package com.ae.springboot303;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {}
---
+++ src/main/java/com/ae/springboot303/HomeController.java
@@ -0,0 +1,34 @@
+package com.ae.springboot303;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    CourseRepository courseRepository;
+
+    @RequestMapping("/")
+    public String listCourses(Model model){
+        model.addAttribute("courses", courseRepository.findAll());
+        return "list";
+    }
+    @GetMapping("/add")
+    public String courseForm(Model model){
+        model.addAttribute("course", new Course());
+        return "courseform";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Course course, BindingResult result){
+        if (result.hasErrors()){
+            return "courseform";
+        }
+    }
+}
