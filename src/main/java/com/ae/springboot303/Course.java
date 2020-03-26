be41dbe4c893eaad86b2d98a5d5b0084c90644f1
Alex Evans <alex.c.evans1@gmail.com>
2020-03-25 20:28:47 -0400

    completed lesson 3.04

--- src/main/java/com/ae/springboot303/Course.java
+++ src/main/java/com/ae/springboot303/Course.java
@@ -30,43 +30,62 @@ public class Course {
     @Min(3)
     private int credit;
 
+    public Course(@NotNull @Size(min = 4) String title, @NotNull @Size(min = 3) String instructor, @NotNull @Size(min = 10) String description, @NotNull @Min(3) int credit) {
+        this.title = title;
+        this.instructor = instructor;
+        this.description = description;
+        this.credit = credit;
+    }
+
+    public Course() {
+    }
+
     public long getId() {
         return id;
     }
 
     public void setId(long id) {
+
         this.id = id;
     }
 
     public String getTitle() {
+
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
     public String getInstructor() {
+
         return instructor;
     }
 
     public void setInstructor(String instructor) {
+
         this.instructor = instructor;
     }
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
     }
 
     public int getCredit() {
+
         return credit;
     }
 
     public void setCredit(int credit) {
+
         this.credit = credit;
     }
 }
